0832d03835d59a632d5f10e5dec2038e5eb85586
tanterajulian <julian.tantera@mi.unc.edu.ar>
2022-03-31 19:34:19 -0300

    obtenemos valor btc y eth

--- src/tp.py
+++ src/tp.py
@@ -3,33 +3,36 @@ import requests
 import ctypes
 
 libcotizacion = ctypes.CDLL('./libcotizacion.so')
-
 libcotizacion.cotizacion.argtypes=(ctypes.c_uint,ctypes.c_uint,)
 libcotizacion.cotizacion.restype=(ctypes.c_uint)
 
-url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=9N3E66AEYSMKXXHT'
-r = requests.get(url)
-data = json.loads(r.text)
-BTCtoUSD = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+def cotizar(cotizacion, precio):
+    return libcotizacion.cotizacion(ctypes.c_uint(cotizacion),ctypes.c_uint(precio))
 
-url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=ARS&apikey=9N3E66AEYSMKXXHT'
-r = requests.get(url)
-data = json.loads(r.text)
-USDtoARS = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+def getprecio(monedacotizar, monedarequerida):
+    rget = requests.get('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency='+monedacotizar+ '&to_currency='+monedarequerida+'&apikey=9N3E66AEYSMKXXHT')
+    dataget = json.loads(rget.text)
+    return dataget['Realtime Currency Exchange Rate']['5. Exchange Rate']
 
-BTCf=float(BTCtoUSD)*10
-USDf=float(USDtoARS)*10
 
-BTCi=int(BTCf)
-USDi=int(USDf)
+BTCtoUSD = getprecio('BTC','USD')
+ETHtoUSD = getprecio('ETH','USD')
+USDtoARS = getprecio('USD','ARS')
+USDtoEUR = getprecio('USD','EUR')
 
-print(BTCf)
-print(USDf)
-print(BTCi)
-print(USDi)
+BTCtoUSDi=int(float(BTCtoUSD)*10)
+ETHtoUSDi=int(float(ETHtoUSD)*10)
+USDtoARSi=int(float(USDtoARS)*10)
+USDtoEURi=int(float(USDtoEUR)*10)
 
-cotizado=libcotizacion.cotizacion(ctypes.c_uint(BTCi),ctypes.c_uint(USDi))
-cotizadocond=cotizado/100
-print("retorno en python:")
-print(cotizado)
-print(cotizadocond)
+print('1 Bitcoin = ' + str(BTCtoUSD) + 'USD')
+BTCtoARS=cotizar(BTCtoUSDi,USDtoARSi)/100
+print('1 Bitcoin = ' + str(BTCtoARS) + 'ARS')
+BTCtoEUR=cotizar(BTCtoUSDi,USDtoEURi)/100
+print('1 Bitcoin = ' + str(BTCtoEUR) + 'EUR')
+
+print('1 Ethereum = ' + str(ETHtoUSD) + 'USD')
+ETHtoARS=cotizar(ETHtoUSDi,USDtoARSi)/100
+print('1 Ethereum = ' + str(ETHtoARS) + 'ARS')
+ETHtoEUR=cotizar(ETHtoUSDi,USDtoEURi)/100
+print('1 Ethereum = ' + str(ETHtoEUR) + 'EUR')
