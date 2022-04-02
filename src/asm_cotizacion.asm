138da9b8a888cfedaa0b8783abe6854603519c0c
sartorigaston <sartorigastonemilio@gmail.com>
2022-04-02 13:11:51 -0300

    readme.md y comentado de codigo

--- src/asm_cotizacion.asm
+++ src/asm_cotizacion.asm
@@ -1,38 +1,22 @@
-
-
-;
-; file: skel.asm
-; This file is a skeleton that can be used to start assembly programs.
-
-%include "asm_io.inc"
 segment .data
-;
-; initialized data is put in the data segment here
-;
 result DW 0
 
-
 segment .bss
-;
-; uninitialized data is put in the bss segment
-;
-
-
- 
 
 segment .text
         global  asm_cotizar
-asm_cotizar:
+asm_cotizar:                      
         enter   0,0               ; setup routine
         pusha
         
+        ;se realiza el calculo con los parametros recibidos a traves del stack
         mov     eax,[ebp + 12]
         mov     ebx,[ebp + 8]
         mul     ebx
         mov     [result], eax
         
         popa
-        mov     eax, [result]            ; return back to C
+        mov     eax, [result]            ; retorno a C a traves del registro EAX
         leave                     
         ret
 
--- src/c_cotizacion.c
+++ src/c_cotizacion.c
@@ -1,11 +1,14 @@
 #include "cdecl.h"
 #include <stdio.h>
 
+//declaracion de modulo asm a invocar, especificando el uso de la convencion de llamadas estandar de C
 unsigned PRE_CDECL asm_cotizar( unsigned int cotizacion, unsigned int precio) POST_CDECL;
 
+//funcion que se usara para la invocacion desde capa superior
 unsigned cotizacion(unsigned int cotizacion, unsigned int precio)
 {
     unsigned ret_status;
-    ret_status = asm_cotizar(cotizacion,precio);
-    return ret_status;
+    //se llama a la capa inferior
+    ret_status = asm_cotizar(cotizacion,precio); //los parametros son cargados en el stack y el retorno a traves de EAX
+    return ret_status; //se retorna el resultado 
 }
